5ab8831f95aea9c127b716ffedd38476d53fafd2
Harrison Cross <github@chimpjuice.com>
2018-04-14 00:38:13 -0400

    another debug build

--- basic_example/App.js
+++ basic_example/App.js
@@ -25,7 +25,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native! Why no updates?
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js - DONE!
