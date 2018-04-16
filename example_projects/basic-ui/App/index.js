07b77384e8da0c2b50e849b85e55a958bf9c067c
Harrison Cross <github@chimpjuice.com>
2018-04-15 23:39:58 -0400

    basic UI example, tweak working example

--- example_projects/basic-ui/App/index.js
+++ example_projects/basic-ui/App/index.js
@@ -1,29 +1,98 @@
 import React, { Component } from 'react';
-import { Container, Header, Title, Button, Left, Right, Body, Icon } from 'native-base';
+import {
+  Container, Button, Icon,
+  Header, Left, Right, Body, Title,
+  Content, Text,
+  Footer, FooterTab
+} from 'native-base';
 
 export default class HeaderExample extends Component {
   async componentWillMount() {
     await Expo.Font.loadAsync({
       'Roboto': require('native-base/Fonts/Roboto.ttf'),
       'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-      'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
+      //'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
     });
   }
 
   render() {
    return (
      <Container>
-       <Header>
+       <Header style={{ backgroundColor: "red" }}>
          <Left>
            <Button transparent>
              <Icon name='menu' />
            </Button>
          </Left>
          <Body>
-           <Title>Header</Title>
+           <Title>Basic UI with NativeBase</Title>
          </Body>
          <Right />
        </Header>
+
+       <Content>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+          <Text>Will It Scroll?</Text>
+        </Content>
+
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Text>Apps</Text>
+            </Button>
+            <Button>
+              <Text>Camera</Text>
+            </Button>
+            <Button active>
+              <Text>Navigate</Text>
+            </Button>
+            <Button>
+              <Text>Contact</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
      </Container>
    );
  }
