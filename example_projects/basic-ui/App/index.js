445382d494fa05d4329e43639a9a67ce9131624c
Harrison Cross <github@chimpjuice.com>
2018-04-15 23:10:01 -0400

    basic UI example, try to fix again

--- example_projects/basic-ui/App/index.js
+++ example_projects/basic-ui/App/index.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 import { Container, Header, Title, Button, Left, Right, Body, Icon } from 'native-base';
 
 export default class HeaderExample extends Component {
- render() {
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
+    });
+  }
+
+  render() {
    return (
      <Container>
        <Header>
